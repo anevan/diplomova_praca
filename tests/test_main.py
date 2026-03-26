59851c7e366bf05d2151b7ddc8eaa5c661acec8f
Aneta Ivaničová <i.anetteanika@gmail.com>
2026-03-26 14:58:25 +0100

    updated the test file

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,29 +1,74 @@
 import pandas as pd
 from unittest.mock import patch
+import pytest
+import matplotlib
 
-@patch('builtins.input', side_effect=[
-    'n',  # cv
-    'n',  # histograms
-    'pearson',  # correlation method
-    '0.1',  # alpha
-    'A','C',  # source/target
-    '0.3',  # frac
-    '1.0',  # C
-    '0.1',  # epsilon
-    'scale',  # gamma
-    '5',  # max_depth
-    '2',  # min_samples_split
-    '1',  # min_samples_leaf
-    'pastel-red',  # palette
-    'y', # DFS: list all chains
-    'n', # append CSV
-    'y' # compute correlation
+matplotlib.use('Agg') # to prevent plots from opening during tests
+
+@pytest.mark.parametrize("side_effects", [
+    [
+        'n',  # cv
+        'n',  # histograms
+        'pearson',  # correlation method
+        '0.1',  # alpha
+        'A','C',  # source/target
+        '0.3',  # frac
+        '1.0',  # C
+        '0.1',  # epsilon
+        'scale',  # gamma
+        '5',  # max_depth
+        '2',  # min_samples_split
+        '1',  # min_samples_leaf
+        'pastel-red',  # palette
+        'y', # DFS: list all chains
+        'n', # append CSV
+        'y' # compute correlation
+    ],
+    [
+        'y',  # cv
+        'y',  # histograms
+        'spearman',  # correlation method
+        '1',  # alpha
+        'B', 'C',  # source / target attributes
+        '0.5',  # LOESS fraction
+        '-1',  # SVR C
+        '0.05',  # SVR epsilon
+        'auto',  # SVR gamma
+        '3',  # max depth (CART)
+        '2',  # min samples to split (CART)
+        '1',  # min samples in leaf (CART)
+        'pastel-orange',  # plot palette
+        'n',  # DFS: list all chains
+        'n',  # append CSV
+        'n'   # compute correlation
+    ],
+    [
+        'n',  # cv
+        'n',  # histograms
+        'kendall',  # correlation method
+        '0.05',  # alpha
+        'C', 'D',  # source / target attributes
+        'A', # correct target
+        '2',  # LOESS fraction
+        '-1',  # SVR C
+        '-1',  # SVR epsilon
+        '-1',  # SVR gamma
+        '-1',  # max depth (CART)
+        '-1',  # min samples to split (CART)
+        '-1',  # min samples in leaf (CART)
+        'bright',  # plot palette
+        'n',  # DFS: list all chains
+        'n',  # append CSV
+        'n'  # compute correlation
+    ]
 ])
-def test_main_pipeline(mock_input):
+def test_main_pipeline(side_effects):
     import main
     main.load_csv = lambda: (pd.DataFrame({
         "A":[1,2,3],
         "B":[2,3,4],
         "C":[3,2,1]
     }), "test_file")
-    main.main()
+
+    with patch('builtins.input', side_effect=side_effects):
+        main.main()
